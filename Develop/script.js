3b1db6b12179a0b7dc70fd904cb5253dbe01a331
Wayne <waynefenwick@gmail.com>
2023-04-27 09:17:13 -0500

    Further updates to coding specifically trying to retrieve weather data

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,35 @@
 const searchBtn = document.querySelector('searchBtn');
 const getWeatherApi = document.querySelector('weatherApi');
 const cityName = document.querySelector('citySearch');
+const cityDetails = document.querySelector('cityDetails');
+
+
+// Step 2-1: Search for a city via API -- not working!!
 const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=conroe&appid=d3fa28b2e5752dcd83a75fd76a5961c3';
+const apiCityUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=d3fa28b2e5752dcd83a75fd76a5961c3';
+const srchCity = document.getElementById('searchBtn');
+
+srchCity.addEventListener('click',
+     function(event) {
+     event.preventDefault();
+     fetch(apiCityUrl)
+     .then(function(response) {
+          return response.json();
+     })
+     .then(function(data) {
+          console.log(data);
+     })
+     .catch(function (error) {
+          console.log(error);
+     });
+});
 
 
-// Step 1: When page loads, retrieve data from local storage
-function renderLocalStorage() {
-let cityList = JSON.parse(localStorage.getItem("savedCities)"));
-     if (cityList !== null) {
-          document.querySelector
-     }
-//     if // there is data, retrieve it
-//      else // ignore it
-}
+// Step 2-2: Search for a city via API
+document.getElementById('searchBtn').addEventListener('click', searchForCity);
 
-function searchForCity() { 
+function searchForCity(event) { 
+     event.preventDefault();
      let city = document.getElementById('citySearch').value;// value = city name you enter into the search bar
      let apiUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=d3fa28b2e5752dcd83a75fd76a5961c3';
 
@@ -27,26 +42,41 @@ function searchForCity() {
           }
      })
      .then(function (data) {
-          console.log(data); // Displays weather data in console.log
+          console.log(data);
      })
      .catch(function (error) {
-          console.log(error); // Displays weather error in console.log
+          console.log(error);
      });
 }
-document.getElementById('searchBtn').addEventListener('click', searchForCity);
-// This function is displaying weather data for conroe in the console.log
-// It is not displaying weather data for the city in the search bar
-// Only shows weather for conroe
+
+// Step 2b: Search for a city via api
+function citySearch(ApiUrl) {
+     fetch(ApiUrl)
+          .then(function (response) {
+          return response.json();
+          })
+          .then(function (data) {
+          console.log(data);
+          })
+     if (response.status === 200) {
+          let responseText = document.getElementById('responseText');
+               if (responseText !== null) {
+          responseText.textContent = response.status;
+          }
+     } else {
+          alert('City not found, try again');
+          //reset search bar
+          // user enters another city name
+          // search button is clicked
+     }
+     return response.json();
+     };
+
 
 // You need to create a function now that takes console.log data and populates id=cityDetails
 // You also need to create a function that saves the found city to the cityOfInterest list
 
-
-
-
 //document.getElementById('cityDetails').innerHTML[0].description;
-
-
 
 // In HTML the button has id="searchCity". When you click this button (onclick), 
 // you want to start a function that will search for the city you
@@ -73,74 +103,30 @@ document.getElementById('searchBtn').addEventListener('click', searchForCity);
      //The code is not part of the function, but an item that initializes the function
      // Another way to put it: when you click the search button, you want it to activate the function
 
+// Step 4: Retrieve current weather data
+function currentCondtions(cityDetails) {
 
-
-// Step 4: Search for a city via api
-
-function citySearch(getApiUrl) {
-     fetch(getApiUrl)
-          .then(function (response) {
-          return response.json();
-          })
-          .then(function (data) {
-          console.log(data);
-          })
-     if (response.status === 200) {
-          let responseText = document.getElementById('responseText');
-               if (responseText !== null) {
-          responseText.textContent = response.status;
-          }
-          const cityName = data.name
-          const popup = document.createElement('div');
-          popup.innerHTML = `<p><strong>${cityName}</strong></p>`;
-          popup.style.position = 'fixed';
-          popup.style.top = '50%';
-          popup.style.left = '50%';
-          popup.style.transform = 'translate(-50%, -50%)';
-          popup.style.zIndex = '9999';
-          popup.style.backgroundColor = 'white';
-          popup.style.padding = '10px';
-          popup.style.border = '1px solid black';
-          popup.style.borderRadius = '5px';
-          popup.style.boxShadow = '0px 0px 10px rgba(0, 0, 0, 0.5)';
-          document.body.appendChild(popup);
-          // add click event listener to popup
-          popup.addEventListener('click', () => {
-               // save to cityOfInterest list
-               // save to local storage
-               // fire "function cityData"
-          document.body.removeChild(popup); 
-          });
-     } else {
-          alert('City not found, try again');
-          // reset search bar
-          // user enters another city name
-          // search button is clicked
+     //   -- populate current weather data for city found, to "id=cityDetails"
+     //   -- populate forecast weather data for city found, to "id=cityForecast"
+     
      }
-     return response.json();
-     };
-citySearch(apiUrl);
+     
+     // Step 5: Retrieve forecast weather data
+     function forecastConditions () {
+          
+     }
 
 
-
-// Step 4: Or, select a saved city
-function citySelect () {// Click on a saved city to fire "function cityData"
+// Step 3: Or, select a saved city
+function citySelect (event) {// Click on a saved city to fire "function cityData"
+     event.preventDefault()
      cityOfInterest.addEventListner('click', cityData);
 //   There can be up to 8 cityOfInterest possibilities
 //   How will cityData know to display the data of the selected city?    
 }
 
 
-
-// Step 5: Retrieve current and forecast weather data
-function cityData() {
-//   -- populate current weather data for city found, to "id=cityDetails"
-//   -- populate forecast weather data for city found, to "id=cityForecast"
-
-}
-
-
-// Step 6: Update selected city's data hourly
+// Step 5: Update selected city's data hourly
 function weatherUpdate () {
      //   -- updates selected city's "id-cityDetails" on the hour
      //   -- updates selected city's "id=cityForecast" on the hour
@@ -149,3 +135,19 @@ function weatherUpdate () {
      //   }, 60 * 60 * 1000); // Run the function every hour
      
 }
+
+// Step 6: Save to local storage if necessary
+function saveToLocalStorage () {
+
+}
+
+
+// Step 1: When page loads, retrieve data from local storage
+function renderLocalStorage() {
+     let cityList = JSON.parse(localStorage.getItem("savedCities)"));
+          if (cityList !== null) {
+               document.querySelector
+          }
+     //     if // there is data, retrieve it
+     //      else // ignore it
+     }
